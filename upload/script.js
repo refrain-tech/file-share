208aa4ad869b287510d773b001100ba2465d5e7b
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-29 23:55:59 +0900

    Update script.js

--- upload/script.js
+++ upload/script.js
@@ -1,27 +1,14 @@
-"use strict";
-firebase.initializeApp({
-	apiKey: "AIzaSyApAMc8cGSt2R8NRZ1BdJVBzj9KPSO-jpM",
-	appId: "1:792165889900:web:0f40d633226439b3cca549",
-	authDomain: "refrain-tech-file-share.firebaseapp.com",
-	databaseURL: "https://refrain-tech-file-share.firebaseio.com",
-	measurementId: "G-703CMR8MCG",
-	messagingSenderId: "792165889900",
-	projectId: "refrain-tech-file-share",
-	storageBucket: "refrain-tech-file-share.appspot.com"
-});
-const database = firebase.database();
-const storage = firebase.storage();
+'use strict';
 let SELECTED_FILE = null;
-picker.addEventListener("change", event => SELECTED_FILE = event.target.files);
-upload.addEventListener("click", event => {
-	if (SELECTED_FILE !== null) {
-		[].slice.call(SELECTED_FILE).forEach(file => {
-			const ref = storage.ref(`uploaded/${file.name.split(".")[0]}`);
-			ref.put(file).then(snapshot => ref.getDownloadURL().then(url => database.ref("share").push({
-				name: file.name,
-				url: url
-			})).catch(console.log)).catch(console.log);
-			});
-		SELECTED_FILE = null;
-	}
+picker.addEventListener('change', event => SELECTED_FILE = event.target.files);
+upload.addEventListener('click', event => {
+  if (SELECTED_FILE === null) return;
+  [].slice.call(SELECTED_FILE).forEach(file => {
+    const ref = storage.ref(`uploaded/${file.name.split('.')[0]}`);
+    ref.put(file).then(snapshot => ref.getDownloadURL().then(url => database.ref('share').push({
+      name: file.name,
+      url: url
+    })).catch(console.error)).catch(console.error);
+  });
+  SELECTED_FILE = null;
 });
