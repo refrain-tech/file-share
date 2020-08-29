f06110402a3eb23a9ac310eb9d84c9737b72666b
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2020-08-29 23:54:15 +0900

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+'use strict';
+firebase.initializeApp({
+  apiKey: 'AIzaSyApAMc8cGSt2R8NRZ1BdJVBzj9KPSO-jpM',
+  appId: '1:792165889900:web:0f40d633226439b3cca549',
+  authDomain: 'refrain-tech-file-share.firebaseapp.com',
+  databaseURL: 'https://refrain-tech-file-share.firebaseio.com',
+  measurementId: 'G-703CMR8MCG',
+  messagingSenderId: '792165889900',
+  projectId: 'refrain-tech-file-share',
+  storageBucket: 'refrain-tech-file-share.appspot.com'
+});
+const database = firebase.database();
