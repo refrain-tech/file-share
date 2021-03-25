fe0e85a5ab158ab131e35cfe7446bbf07a2bba6b
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2021-03-25 17:22:41 +0900

    Update script.js

--- download/script.js
+++ download/script.js
@@ -2,8 +2,8 @@
 database.ref('share').once('value', snapshot => {
   const val = snapshot.val() ?? {};
   Object.values(val).forEach(value => {
-    const { name, url } = value;
-    const [ li, a ] = [ 'li', 'a' ].map(tag => document.createElement(tag));
+    const {name, url} = value;
+    const [li, a] = [ 'li', 'a' ].map(tag => document.createElement(tag));
     a.download = name;
     a.href = url;
     a.textContent = name;
