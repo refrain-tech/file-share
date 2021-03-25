0721815b1ead3989cf7d4a3b4ea868901780835d
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2021-03-25 23:31:38 +0900

    Update script.js

--- upload/script.js
+++ upload/script.js
@@ -1,11 +1,10 @@
 'use strict';
 let SELECTED_FILE = null;
 picker.addEventListener('change', ({target: {files}}) => SELECTED_FILE = files, false);
-upload.addEventListener('click', event => {
-  if (SELECTED_FILE === null) return;
-  [].slice.call(SELECTED_FILE).forEach(file => {
+upload.addEventListener('click', () => {
+  [].slice.call(SELECTED_FILE ?? []).forEach(file => {
     const {name} = file;
-    const ref = storage.ref(`uploaded/${name}`);
+    const ref = storage.ref(`files/${name}`);
     ref.put(file).then(snapshot => ref.getDownloadURL().then(url => database.ref('share').push({name, url})));
   });
   SELECTED_FILE = null;
