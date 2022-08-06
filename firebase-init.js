8ae4f67651f74783d122fe2a195d26974fc86de1
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2022-08-06 19:53:27 +0900

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,13 +1,13 @@
 'use strict';
 firebase.initializeApp({
-  apiKey: 'AIzaSyApAMc8cGSt2R8NRZ1BdJVBzj9KPSO-jpM',
-  appId: '1:792165889900:web:0f40d633226439b3cca549',
-  authDomain: 'refrain-tech-file-share.firebaseapp.com',
-  databaseURL: 'https://refrain-tech-file-share.firebaseio.com',
-  measurementId: 'G-703CMR8MCG',
-  messagingSenderId: '792165889900',
-  projectId: 'refrain-tech-file-share',
-  storageBucket: 'refrain-tech-file-share.appspot.com'
+  apiKey: 'AIzaSyC8CYEPcEHsmV0KHbWqg_2MC07jPi5si4o',
+  appId: '1:151384778320:web:370e6dd815b172d5805355',
+  authDomain: 'file-share-e2e18.firebaseapp.com',
+  databaseURL: 'https://file-share-e2e18-default-rtdb.firebaseio.com',
+  measurementId: 'G-4N6VX24KKD',
+  messagingSenderId: '151384778320',
+  projectId: 'file-share-e2e18',
+  storageBucket: 'file-share-e2e18.appspot.com'
 });
 const database = firebase.database();
 const storage = firebase.storage();
