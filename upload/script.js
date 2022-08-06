197371ae13a998fba6e917c2ca0eb643fb1c8c3c
Refrain Tech <59395606+refrain-tech@users.noreply.github.com>
2022-08-06 19:44:49 +0900

    Update script.js

--- upload/script.js
+++ upload/script.js
@@ -5,7 +5,10 @@ upload.addEventListener('click', () => {
   [].slice.call(SELECTED_FILE ?? []).forEach(file => {
     const {name} = file;
     const ref = storage.ref(`files/${name}`);
-    ref.put(file).then(snapshot => ref.getDownloadURL().then(url => database.ref('share').push({name, url})));
+    ref.put(file).then(snapshot => ref.getDownloadURL().then(url => {
+      database.ref('share').push({name, url});
+      window.alert(`password: ${Date.now()}`);
+    }));
   });
   SELECTED_FILE = null;
 }, false);
